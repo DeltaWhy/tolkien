4cee3cfcf4c841aa19a1faaa5bfbf138f7d753a1
Michael Limiero <mike5713@gmail.com>
2016-01-07 20:45:27 -0500

    Add layout and views

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tolkien
 One account to rule them all
+
+## Setup
+```
+$ npm install
+```
+
+## Running
+```
+$ npm start
+```
+
+To automatically reload the app when a file changes, run with [nodemon](https://github.com/remy/nodemon).
+```
+$ npm install -g nodemon
+$ nodemon ./bin/www
+```
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tolkien' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('index', { title: 'Tolkien' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Tolkien' });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'Tolkien' });
+});
+
+router.get('/logout', function(req, res, next) {
+  res.render('logout', { title: 'Tolkien' });
 });
 
 module.exports = router;
